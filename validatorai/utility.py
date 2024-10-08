4e1691b5319c43d4a7664639955cdff3adc7fc79
okondev2021 <sojapril16@gmail.com>
2024-10-08 11:54:35 +0100

    stream test db update

--- validatorai/utility.py
+++ validatorai/utility.py
@@ -74,10 +74,6 @@ def generate_response(idea, target_market, timeout=30):
     f"Target Market {target_market}",
     "Idea Validation ",
   ], stream=True)
-  # Convert markdown response to HTML
-  # response_html_content = markdown.markdown(response.text)
-  # formatted_response_html_content = response_html_content.replace('\n', '')
-  # return formatted_response_html_content 
   
   return response
   
