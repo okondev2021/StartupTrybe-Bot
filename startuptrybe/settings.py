42a04d2345c7e35a39f6914d2573b7bf083cb4e8
okondev2021 <sojapril16@gmail.com>
2024-10-27 15:14:20 +0100

    updated my allowed host

--- startuptrybe/settings.py
+++ startuptrybe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'startuptrybe-bot.onrender.com']
 
 
 # Application definition
