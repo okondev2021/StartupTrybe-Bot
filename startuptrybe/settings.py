106111faa259bc0de878c044895e4da1e49b4b7c
okondev2021 <sojapril16@gmail.com>
2024-10-27 13:39:33 +0100

    adjusted duration

--- startuptrybe/settings.py
+++ startuptrybe/settings.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=8),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": True,
     'BLACKLIST_AFTER_ROTATION': True,
 
